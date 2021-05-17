c9ecbeed3a874cf2900d1774b612e4a50738f261
Jason Deng <foralief@gmail.com>
2021-05-16 22:37:53 -0500

    Continue working on Chapter 4

---
+++ SpeedConverter/src/FileWriteDemo.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class FileWriteDemo {
+    public static void main(String[] args) throws IOException {
+        String filename;
+        String friendName;
+        int numFriends;
+
+        // Create a Scanner object for keyboard input.
+        Scanner keyboard = new Scanner(System.in);
+
+        // Get the number of friends.
+        System.out.print("How many friedns do you have? ");
+        numFriends = keyboard.nextInt();
+
+        //  Consume the remaining newline character.
+        keyboard.nextLine();
+
+        // Get the filename.
+        System.out.print("Enter the filename: ");
+        filename = keyboard.nextLine();
+
+        // Open the file.
+        PrintWriter outputFile = new PrintWriter(filename);
+
+        // Get data and write it to the file.
+        for (int i = 1; i <= numFriends; i++) {
+            // Get the name of a friend.
+            System.out.print("Enter the name of friend number " + i + ": ");
+            friendName = keyboard.nextLine();
+
+            // Write the name to the file.
+            outputFile.println(friendName);
+        }
+
+        // Close the file.
+        outputFile.close();
+        System.out.println("Data written to the file.");
+    }
+}
---
+++ SpeedConverter/src/StairStepPattern.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class StairStepPattern {
+    public static void main(String[] args) {
+        final int NUM_STEPS = 6;
+
+        for (int r = 0; r < NUM_STEPS; r++) {
+            for (int c = 0; c < r; c++) {
+                System.out.print(" ");
+            }
+            System.out.println("#");
+        }
+    }
+}
