bf923f29cfc7eb4e76e5461956062b2622d286ba
Jason Deng <foralief@gmail.com>
2021-06-30 21:01:08 -0500

    Finished up to Code Listing 8-4 MetricDemo.java

---
+++ Chapter8/src/Metric.java
@@ -0,0 +1,13 @@
+/**
+ * This class demonstrates static methods.
+ */
+
+public class Metric {
+    public static double milesToKilometers(double m) {
+        return m * 1.609;
+    }
+
+    public static double kilometersToMiles(double k) {
+        return k / 1.609;
+    }
+}
---
+++ Chapter8/src/MetricDemo.java
@@ -0,0 +1,33 @@
+import javax.swing.JOptionPane;
+
+/**
+ * This program demonstrates the Metric class.
+ */
+
+public class MetricDemo {
+    public static void main(String[] args) {
+        String input;
+        double miles;
+        double kilos;
+
+        // Get a distance in miles.
+        input = JOptionPane.showInputDialog("Enter a distance in miles.");
+        miles = Double.parseDouble(input);
+
+        // Convert the distance to kilometers.
+        kilos = Metric.milesToKilometers(miles);
+        JOptionPane.showMessageDialog(null, String.format("%,.2f miles equals %,.2f kilometers.",
+                miles, kilos));
+
+        // Get a distance in kilometers.
+        input = JOptionPane.showInputDialog("Enter a distance in kilometers: ");
+        kilos = Double.parseDouble(input);
+
+        // Convert the distance to miles.
+        miles = Metric.kilometersToMiles(kilos);
+        JOptionPane.showMessageDialog(null, String.format("%,.2f kilometers equals %,.2f mils.",
+                kilos, miles));
+
+        System.exit(0);
+    }
+}
