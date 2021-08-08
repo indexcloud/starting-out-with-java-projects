7d588e8b7a5fcbf2ad894db7633564226fd4fd6a
Jason Deng <foralief@gmail.com>
2021-08-08 12:51:02 -0500

    Finished up to Code Listing 11-1 BadArray.java

---
+++ Chapter11/src/BadArray.java
@@ -0,0 +1,38 @@
+/**
+ * This program causes an error and crashes.
+ */
+
+public class BadArray {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+
+        for (int i = 0; i <= 3; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
